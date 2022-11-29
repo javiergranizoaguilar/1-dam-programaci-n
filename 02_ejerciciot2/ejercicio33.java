ed3bddedffa50229a16f3b767bebf5117a3703e0
javier granizo <114944864+javiergranizoaguilar@users.noreply.github.com>
2022-11-29 16:35:32 +0100

    Update ejercicio33.java

--- 02_ejerciciot2/ejercicio33.java
+++ 02_ejerciciot2/ejercicio33.java
@@ -14,19 +14,31 @@ public class Tema3ejercicio33 {
     public static void main(String[] args) {
         PlayList a=null;
         a=new PlayList();
-        ArchivoMP3 b=new ArchivoMP3("C:\\Users\\javie\\OneDrive\\Escritorio\\mp3 eje2\\CuartetodeNosElhijodeHernández.mp3");
-        ArchivoMP3 c=new ArchivoMP3("C:\\Users\\javie\\OneDrive\\Escritorio\\mp3 eje2\\El Cuarteto de Nos - Mario Neta (Official Video).mp3");
-        ArchivoMP3 d=new ArchivoMP3("C:\\Users\\javie\\OneDrive\\Escritorio\\mp3 eje2\\Five Nights at Freddy's 4 Song - I Got No Time (FNAF4) - The Living Tombstone.mp3");
+        ArchivoMP3 b=new ArchivoMP3("C:\\Users\\javie\\OneDrive"
+                + "\\Escritorio\\mp3 eje2\\CuartetodeNosElhijodeHernández.mp3");
+        
+        ArchivoMP3 c=new ArchivoMP3("C:\\Users\\javie\\OneDrive\\Escritorio"
+                + "\\mp3 eje2\\El Cuarteto de Nos - Mario Neta (Official Video).mp3");
+        
+        ArchivoMP3 d=new ArchivoMP3("C:\\Users\\javie\\OneDrive\\Escritorio\\mp3 "
+                + "eje2\\Five Nights at Freddy's 4 Song - I Got No Time (FNAF4)"
+                + " - The Living Tombstone.mp3");
+        
         a.añadir(b);
+        
         a.añadir(c);
+        
         a.añadir(d);
+        
         a.setTitulo("el musico");
-        try {a.guardar("C:\\Users\\javie\\OneDrive\\Escritorio\\si");}
+        try {
+            a.guardar("C:/Users/javie/OneDrive/Escritorio/music.txt");
+            
+            Reproductor e=new Reproductor(a,true,false);
+            e.play();
+        }
         catch (Exception IOException){
         System.out.println("no se a podido realizar");}
-        Reproductor e=new Reproductor(b,true,false);
-        Reproductor f=new Reproductor(c,true,false);
-        Reproductor g=new Reproductor(d,true,false);
         
     }
 
