b97131adfb86fcb910c33d65766435255bd77eac
javier granizo <114944864+javiergranizoaguilar@users.noreply.github.com>
2022-11-29 18:00:48 +0100

    Add files via upload

---
+++ 02_ejerciciot2/Tema2ejercicio43.java
@@ -0,0 +1,21 @@
+
+
+package tema2ejercicio43;
+import java.text.*;
+/**
+ *
+ * @author javier granizo <javiercelia123@gmail.com>
+ */
+public class Tema2ejercicio43 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        double x=4*((Math.atan(1)));
+        
+        System.out.println(String.format("%.4f",x));
+    }
+
+}
