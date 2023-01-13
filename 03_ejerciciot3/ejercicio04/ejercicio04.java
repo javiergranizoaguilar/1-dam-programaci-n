32e4a614462806d3914258151013eb7620f3e2cc
javier granizo <114944864+javiergranizoaguilar@users.noreply.github.com>
2023-01-13 17:07:19 +0100

    Create ejercicio04.java

---
+++ 03_ejerciciot3/ejercicio04/ejercicio04.java
@@ -0,0 +1,53 @@
+package tema03eje04;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author javier granizo <javiercelia123@gmail.com>
+ */
+public class Tema03eje04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        boolean r=true;
+        
+        while(r){
+        
+            System.out.println("Eres mayor de edad ?");
+            System.out.println("Escribe Y para si");
+            System.out.println("Escribe N para no");
+            
+            char s=new Scanner(System.in).next().charAt(0);
+            
+            if(s=='Y'){
+                
+                System.out.println("Eres mayor de edad");
+            
+                r=false;
+            
+            }
+            else{
+            
+                if(s=='N'){
+                
+                    System.out.println("No eres mayor de edad");
+            
+                    r=false;
+            
+                }
+                else{
+                
+                    System.out.println("No se a identificado la letra");
+                    System.out.println("Recuerda poner N o Y en mayuscula");
+                
+                }
+            }
+        
+        }
+    }
+
+}
