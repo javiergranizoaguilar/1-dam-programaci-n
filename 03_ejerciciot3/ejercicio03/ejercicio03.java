f3c7ddc63df35175f1ff7df21bd38c9130ca6128
javier granizo <114944864+javiergranizoaguilar@users.noreply.github.com>
2023-01-13 16:55:13 +0100

    Create ejercicio03.java

---
+++ 03_ejerciciot3/ejercicio03/ejercicio03.java
@@ -0,0 +1,36 @@
+package tema3.eje03;
+
+/**
+ *
+ * @author javier granizo <javiercelia123@gmail.com>
+ */
+public class Tema3Eje03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        boolean r=true;
+        
+        while(r){
+        
+            int numr=(int)(Math.random()*10+1);
+            
+            System.out.println(numr);
+            
+            if(numr==10){
+            
+                r=false;
+            
+            }
+            else{
+            
+                
+            
+            }
+        
+        }
+    }
+
+}
