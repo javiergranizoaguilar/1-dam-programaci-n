c26583c5798c64f3d4c5792db631e8458dd787f9
javier granizo <114944864+javiergranizoaguilar@users.noreply.github.com>
2023-01-30 20:12:09 +0100

    Update 01_ejemplo.java

--- 04_ejemplo/01_ejemplo.java
+++ 04_ejemplo/01_ejemplo.java
@@ -16,12 +16,12 @@ public class Ejemplo1t4 {
         cafetera c=new cafetera(100,1);
         
         System.out.println("norma");
-        System.out.println("a= "+a.getmax());
-        System.out.println("a= "+a.getactual());
-        System.out.println("b= "+b.getmax());
-        System.out.println("c= "+c.getmax());
-        System.out.println("b= "+b.getactual());
-        System.out.println("c= "+c.getactual());
+        System.out.println("a max= "+a.getmax());
+        System.out.println("a actual= "+a.getactual());
+        System.out.println("b max= "+b.getmax());
+        System.out.println("c max= "+c.getmax());
+        System.out.println("b actual= "+b.getactual());
+        System.out.println("c actual= "+c.getactual());
         
         a.setmax(100);
         a.setactual(10);
@@ -31,61 +31,62 @@ public class Ejemplo1t4 {
         c.setactual(103);
         
         System.out.println("canvios en max y en actual");
-        System.out.println("a= "+a.getmax());
-        System.out.println("a= "+a.getactual());
-        System.out.println("b= "+b.getmax());
-        System.out.println("c= "+c.getmax());
-        System.out.println("b= "+b.getactual());
-        System.out.println("c= "+c.getactual());
+        System.out.println("a max= "+a.getmax());
+        System.out.println("a actual= "+a.getactual());
+        System.out.println("b max= "+b.getmax());
+        System.out.println("c max= "+c.getmax());
+        System.out.println("b actual= "+b.getactual());
+        System.out.println("c actual= "+c.getactual());
         
         a.llenarcafetera();
         b.llenarcafetera();
         c.llenarcafetera();
         
         System.out.println("llenar");
-        System.out.println("a= "+a.getmax());
-        System.out.println("a= "+a.getactual());
-        System.out.println("b= "+b.getmax());
-        System.out.println("c= "+c.getmax());
-        System.out.println("b= "+b.getactual());
-        System.out.println("c= "+c.getactual());
+        System.out.println("a max= "+a.getmax());
+        System.out.println("a actual= "+a.getactual());
+        System.out.println("b max= "+b.getmax());
+        System.out.println("c max= "+c.getmax());
+        System.out.println("b actual= "+b.getactual());
+        System.out.println("c actual= "+c.getactual());
         
         a.servirtaza(20);
         b.servirtaza(10);
         c.servirtaza(10);
         
         System.out.println("servir");
-        System.out.println("a= "+a.getmax());
-        System.out.println("a= "+a.getactual());
-        System.out.println("b= "+b.getmax());
-        System.out.println("c= "+c.getmax());
-        System.out.println("b= "+b.getactual());
-        System.out.println("c= "+c.getactual());
+        System.out.println("a max= "+a.getmax());
+        System.out.println("a actual= "+a.getactual());
+        System.out.println("b max= "+b.getmax());
+        System.out.println("c max= "+c.getmax());
+        System.out.println("b actual= "+b.getactual());
+        System.out.println("c actual= "+c.getactual());
         
         a.vaciar();
         b.vaciar();
         c.vaciar();
         
         System.out.println("vaciar");
-        System.out.println("a= "+a.getmax());
-        System.out.println("a= "+a.getactual());
-        System.out.println("b= "+b.getmax());
-        System.out.println("c= "+c.getmax());
-        System.out.println("b= "+b.getactual());
-        System.out.println("c= "+c.getactual());
+        System.out.println("a max= "+a.getmax());
+        System.out.println("a actual= "+a.getactual());
+        System.out.println("b max= "+b.getmax());
+        System.out.println("c max= "+c.getmax());
+        System.out.println("b actual= "+b.getactual());
+        System.out.println("c actual= "+c.getactual());
         
         a.agregarcafe(1000);
         b.agregarcafe(1000);
         c.agregarcafe(20);
         
         System.out.println("agregar");
-        System.out.println("a= "+a.getmax());
-        System.out.println("a= "+a.getactual());
-        System.out.println("b= "+b.getmax());
-        System.out.println("c= "+c.getmax());
-        System.out.println("b= "+b.getactual());
-        System.out.println("c= "+c.getactual());
+        System.out.println("a max= "+a.getmax());
+        System.out.println("a actual= "+a.getactual());
+        System.out.println("b max= "+b.getmax());
+        System.out.println("c max= "+c.getmax());
+        System.out.println("b actual= "+b.getactual());
+        System.out.println("c actual= "+c.getactual());
        
     }
 
 }
+
