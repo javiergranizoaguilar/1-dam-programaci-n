ec2f6f4653cc29f1a8dd7f143bbe0920c8f21652
javier granizo <114944864+javiergranizoaguilar@users.noreply.github.com>
2023-01-30 21:12:21 +0100

    Create 01_ejercicio.java

---
+++ 04_ejerciciot4/01_ejercicio.java
@@ -0,0 +1,25 @@
+package objeto;
+
+/**
+ *
+ * @author javier granizo <javiercelia123@gmail.com>
+ */
+public class DNI {
+    
+    private int numero;
+    private char letra;
+    private String frase;
+    
+    public DNI(int n,char i){
+    
+        numero=n;
+        letra=i;
+    
+    };
+    public DNI(String a){
+    
+        letra=a.charAt(8);
+        numero=Integer.parseInt(a.substring(0, 7)); 
+    };
+    
+}
