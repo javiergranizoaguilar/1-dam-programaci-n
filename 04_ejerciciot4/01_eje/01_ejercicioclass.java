2f57b4db95e76a43ff5afec9916d93c9359e41c7
javier granizo <114944864+javiergranizoaguilar@users.noreply.github.com>
2023-01-31 16:48:37 +0100

    Update 01_ejercicioclass.java

--- 04_ejerciciot4/01_eje/01_ejercicioclass.java
+++ 04_ejerciciot4/01_eje/01_ejercicioclass.java
@@ -8,18 +8,33 @@ public class DNI {
     
     private int numero;
     private char letra;
-    private String frase;
     
     public DNI(int n,char i){
     
         numero=n;
         letra=i;
     
-    };
+    }
     public DNI(String a){
     
         letra=a.charAt(7);
         numero=Integer.parseInt(a.substring(0, 6)); 
-    };
+    }
     
+    public int getnum(){
+    
+        return numero;
+    }
+    public char getletra(){
+    
+        return letra;
+    }
+    public void setnum(int a){
+    
+        numero=a;
+    }
+    public void setletra(char a){
+    
+        letra=a;
+    }
 }
