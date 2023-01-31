43996148eb192a3e40b70aa37b11652e97d2d1ad
javier granizo <114944864+javiergranizoaguilar@users.noreply.github.com>
2023-01-31 17:02:25 +0100

    Update 02_ejercicioclass.java

--- 04_ejerciciot4/02_eje/02_ejercicioclass.java
+++ 04_ejerciciot4/02_eje/02_ejercicioclass.java
@@ -4,24 +4,41 @@ package objeto;
  *
  * @author javier granizo <javiercelia123@gmail.com>
  */
-public class caja {
+public class Caja {
     
     private boolean abierto;
     private String mensaje;
     
-    public caja(String a){
-    
-        mensaje=a;
-    };
-    public caja(boolean a,String b){
-    
-        abierto=a;
-        mensaje=b;
-    };
-    public caja(){
+    public Caja(){
     
         abierto=false;
         mensaje="Viva el tema 7";
-    };
+    }
+    public Caja(String a){
+    
+        mensaje=a;
+    }
+    public Caja(boolean a,String b){
+    
+        abierto=a;
+        mensaje=b;
+    }
+    
+    public boolean getabierto(){
+    
+        return abierto;
+    }
+    public String getmensaje(){
+    
+        return mensaje;
+    }
+    public void setabierto(boolean a){
+    
+        abierto=a;
+    }
+    public void setmensaje(String a){
+    
+        mensaje=a;
+    }
     
 }
