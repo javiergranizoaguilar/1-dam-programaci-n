6a0c8a16d3e49d25c9a2e64f5f92c0248e4aa278
javier granizo <114944864+javiergranizoaguilar@users.noreply.github.com>
2023-03-06 21:37:34 +0100

    Create CasaRural.java

---
+++ 05_ejerciciot5/eje6/CasaRural.java
@@ -0,0 +1,13 @@
+package objetos;
+
+/**
+ *
+ * @author javier granizo <javiercelia123@gmail.com>
+ */
+public class CasaRural extends Hotel {
+    
+    public CasaRural(String d) {
+        super(d,3,6);
+    }
+    
+}
