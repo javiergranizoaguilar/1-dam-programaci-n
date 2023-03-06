5a9b2645c467f1dcc9d29ba4dff41b101b12adbc
javier granizo <114944864+javiergranizoaguilar@users.noreply.github.com>
2023-03-06 21:35:12 +0100

    Create Hotel.java

---
+++ 05_ejerciciot5/eje6/Hotel.java
@@ -0,0 +1,61 @@
+package objetos;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+/**
+ *
+ * @author javier granizo <javiercelia123@gmail.com>
+ */
+public class Hotel extends Edificio {
+
+    private List<String> clientes;
+    private int maximo;
+
+    /**
+     *
+     * @param d direcion de el hotel
+     * @param np numero de plantas de el hotel
+     * @param m numero maximo de clientes
+     */
+    public Hotel(String d, int np, int m) throws IllegalArgumentException {
+        super(d, np);
+        if(m<0){
+        
+            throw new IllegalArgumentException(); 
+        }
+        
+        this.maximo = m;
+    }
+
+    /**
+     *
+     * @param c nombre de el cliente a añadir
+     */
+    public void añadirCliente(String c) throws IllegalStateException{
+        
+        if(this.clientes.size()>this.maximo){
+        
+            throw new IllegalStateException(); 
+        }
+
+        this.clientes.add(c);
+    }
+
+    /**
+     *
+     * @param c nombre del cliente a quitar
+     */
+    public void retirarCliente(String c) throws NoSuchElementException{
+        
+        if(this.clientes.contains(c)){
+
+        this.clientes.remove(c);
+        }
+        else{
+        
+            throw new NoSuchElementException();
+        }
+    }
+
+}
