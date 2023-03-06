962cdd0e7c426e551b3006bc1cf11751b3bb03ac
javier granizo <114944864+javiergranizoaguilar@users.noreply.github.com>
2023-03-06 21:38:31 +0100

    Create Rascacielos.java

---
+++ 05_ejerciciot5/eje6/Rascacielos.java
@@ -0,0 +1,35 @@
+package objetos;
+
+/**
+ *
+ * @author javier granizo <javiercelia123@gmail.com>
+ */
+public class Rascacielos extends Edificio {
+
+    private int altura;
+
+    /**
+     *
+     * @param d direccion del rascacielos
+     * @param np numero de plantas del racacielos
+     * @param a altura del rascacielos
+     */
+    public Rascacielos(String d, int np, int a) {
+        super(d, np);
+        if(a<0){
+        
+            throw new IllegalArgumentException();
+        }
+        this.altura = 10+(a*3);
+    }
+
+    /**
+     *
+     * @return debuellve la altura del racacielos
+     */
+    public int getAltura() {
+
+        return this.altura;
+    }
+
+}
